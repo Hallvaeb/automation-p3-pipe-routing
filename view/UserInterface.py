cddf5167f329c0722383f87714c8db0d8c16090b
Hallvaeb <hallvard.bjorgen@gmail.com>
2022-04-26 14:11:04 +0200

    make folders

---
+++ view/UserInterface.py
@@ -0,0 +1,15 @@
+"""
+Here you can give your input parameters. Run this class to create your solution.
+Solution will be added to the 'working_dir_folder'.
+"""
+
+working_dir_folder = "C:\Users\Eier\Github\kbe-a3\WorkingDirectory"
+
+param1 = ""
+param2 = ""
+param3 = ""
+param4 = ""
+param5 = ""
+param6 = ""
+param7 = ""
+
