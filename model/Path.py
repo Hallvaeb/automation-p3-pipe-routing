13efa8ab37356e41b9d26b0f90e5caa4fa96323b
johagl <johagl@stud.ntnu.no>
2022-04-26 16:05:53 +0200

    small change path

--- model/Path.py
+++ model/Path.py
@@ -21,9 +21,10 @@ class Path():
         # [(xA,yA,zA), (x1,y1,z1), (x2,y2,z2), (xB, yB, zB)] (Kun ett equipment)
         # Blir da path mellom punkt A-1. og punkt 2-B. Mellom 1 og 2 er det et equipment
         
-        list_of_paths = [] #vil bli: [[path1], [path2]] = [[pointA, point1], [point2, pointB]]
-        for i in range(0, len(self.points_to_visit)):
-            list_of_paths.append([points_to_visit i])
+        # Dette er ikke ferdig, kun en start på en tankegang..
+        # list_of_paths = [] #vil bli: [[path1], [path2]] = [[pointA, point1], [point2, pointB]]
+        # for i in range(0, len(self.points_to_visit)): #må få denne til å lage path mellom to og to
+        #     list_of_paths.append([points_to_visit])
         
         return list_of_paths
 
