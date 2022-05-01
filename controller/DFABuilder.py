f3cff3078fdb537a0bbe23d2f87e52bd4fc730a7
SigveSjo <sigvesjo@gmail.com>
2022-05-01 21:08:32 +0200

    s

--- controller/DFABuilder.py
+++ controller/DFABuilder.py
@@ -4,7 +4,11 @@ from model.Environment import Environment
 from model.Equipment import Equipment
 from model.PathGenerator import PathGenerator
 from model.Pipe import Pipe
+from controller.IDGenerator import IDGenerator
+import pathlib
 
+
+path_to_dfa_folder = str(pathlib.Path().absolute().as_posix())+"/DFAs/"
 
 class DFABuilder():
 
@@ -12,10 +16,11 @@ class DFABuilder():
 	def generate_dfa(env, equs, pipe, complete_paths):
 		""" Generates the DFA file. 
 		Input: Environment, List<Equipment>, Pipe, List<List<tuples/point>>. """
+		DFABuilder.append_pipe_to_DFA(pipe)
 		DFABuilder.append_env_to_DFA(env)
 		for equ in equs:
 			DFABuilder.append_equ_to_DFA(equ)
-		DFABuilder.append_pipe_to_DFA(pipe)
+		
 		for path in complete_paths:
 			DFABuilder.append_path_to_DFA(path)
 		DFABuilder.sweep_paths()
@@ -32,12 +37,27 @@ class DFABuilder():
 
 	def append_pipe_to_DFA(pipe):
 		""" Append pipe to the current DFA file """
-		f = open("templates/Pipe.dfa", "r")
+		design_id = IDGenerator.create_design_ID()
+		f = open(path_to_dfa_folder + "templates/PipeSystem.dfa", "r")
 		txt = f.read()
-		txt = txt.replace("<CURVE_RADIUS>", pipe.elbow_curve_radius)
-		txt = txt.replace("<OUTER_RADIUS>", pipe.diameter_outer)
-		txt = txt.replace("<INNER_RADIUS>", pipe.diameter_inner)
-		pass
+		txt = txt.replace("<ID>", design_id)
+		f.close()
+		f = open(path_to_dfa_folder + "products/" + design_id + ".dfa", "w")
+		f.write(txt)
+		f.close
+
+		f1 = open(path_to_dfa_folder + "templates/Pipe.dfa", "r")
+		txt1 = f1.read()
+		txt1 = txt1.replace("<CURVE_RADIUS>", pipe.elbow_curve_radius)
+		txt1 = txt1.replace("<OUTER_RADIUS>", pipe.diameter_outer)
+		txt1 = txt1.replace("<INNER_RADIUS>", pipe.diameter_inner)
+		f1.close()
+
+		f1 = open(path_to_dfa_folder + "products/" + design_id + ".dfa", "a")
+		f1.write(txt1)
+		f1.close
+		return design_id
+
 
 
 
@@ -72,3 +92,6 @@ class DFABuilder():
 		""" Sweeps on the paths to make the pipes """
 		# TODO: implement
 		pass
+
+p1 = Pipe(25, 20, 90)
+DFABuilder.append_pipe_to_DFA(p1)
--- model/Pipe.py
+++ model/Pipe.py
@@ -3,6 +3,7 @@
 class Pipe():
 
 
-    def __init__(self, diameter_outer, diameter_inner): 
+    def __init__(self, diameter_outer, diameter_inner, curve_radius): 
         self.diameter_outer = diameter_outer
         self.diameter_inner = diameter_inner
+        self.curve_radius = curve_radius
