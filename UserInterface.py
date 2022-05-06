a70a83adb81f059e62b4ef9a24c443375de5df52
johagl <johagl@stud.ntnu.no>
2022-05-06 09:33:51 +0200

    updates UserInterface.py with right input fields

--- UserInterface.py
+++ UserInterface.py
@@ -7,64 +7,68 @@ Solution will be added to the 'working_dir_folder' and sent to your email.
 
 # INPUT BEGIN ------------------------------------------------------------------
 
-# #TWO ELEMENTS IN A STRAIGHT LINE
-# # Environment
-# env_pos = (0, 0, 0)
-# env_point_A = (0, 5000, 5000)
-# env_point_A_dir = (1,0,0)
-# env_point_B = (10000, 5000, 5000)
-# env_point_B_dir = (1,0,0)
-# env_length = 10000
-# env_width = 10000
-# env_height = 10000
-
-# # Equipment 1
-# equ1_pos = (2500, 4500, 4500)
-# equ1_point_in = (2500, 5000, 5000)
-# equ1_point_out = (3500, 5000, 5000)
-# equ1_length = 1000
-# equ1_width = 1000
-# equ1_height = 1000
-
-# # Equipment 2
-# equ2_pos = (5500, 4500, 4500)
-# equ2_point_in = (5500, 5000, 5000)
-# equ2_point_out = (6000, 5000, 5000)
-# equ2_length = 500
-# equ2_width = 1000
-# equ2_height = 1000
-
-
-# TWO ELEMENTS IN DIFFERENT X AND Z-COORDS.
+#TWO ELEMENTS IN A STRAIGHT LINE
 # Environment
 env_pos = (0, 0, 0)
-env_point_A = (0, 5000, 4500)
+env_point_A = (0, 5000, 5000)
 env_point_A_dir = (1,0,0)
-env_point_B = (10000, 5000, 8500)
+env_point_B = (10000, 5000, 5000)
 env_point_B_dir = (1,0,0)
 env_length = 10000
 env_width = 10000
 env_height = 10000
 
 # Equipment 1
-equ1_pos = (1500, 4500, 4000)
-equ1_point_in = (1500, 5000, 4500)
+equ1_pos = (2500, 4500, 4500)
+equ1_point_in = (2500, 5000, 5000)
 equ1_point_in_dir=(1,0,0)
-equ1_point_out = (2500, 5000, 4500)
+equ1_point_out = (3500, 5000, 5000)
 equ1_point_out_dir=(1,0,0)
 equ1_length = 1000
 equ1_width = 1000
 equ1_height = 1000
 
 # Equipment 2
-equ2_pos = (6000, 4500, 8000)
-equ2_point_in = (6500, 5000, 8000)
-equ2_point_in_dir=(0,0,1)
-equ2_point_out = (7000, 5000, 8500)
+equ2_pos = (5500, 4500, 4500)
+equ2_point_in = (5500, 5000, 5000)
+equ2_point_in_dir=(1,0,0)
+equ2_point_out = (6000, 5000, 5000)
 equ2_point_out_dir=(1,0,0)
-equ2_length = 1000
+equ2_length = 500
 equ2_width = 1000
 equ2_height = 1000
+
+
+# # TWO ELEMENTS IN DIFFERENT X AND Z-COORDS.
+# # Environment
+# env_pos = (0, 0, 0)
+# env_point_A = (0, 5000, 4500)
+# env_point_A_dir = (1,0,0)
+# env_point_B = (10000, 5000, 8500)
+# env_point_B_dir = (1,0,0)
+# env_length = 10000
+# env_width = 10000
+# env_height = 10000
+
+# # Equipment 1
+# equ1_pos = (1500, 4500, 4000)
+# equ1_point_in = (1500, 5000, 4500)
+# equ1_point_in_dir=(1,0,0)
+# equ1_point_out = (2500, 5000, 4500)
+# equ1_point_out_dir=(1,0,0)
+# equ1_length = 1000
+# equ1_width = 1000
+# equ1_height = 1000
+
+# # Equipment 2
+# equ2_pos = (6000, 4500, 8000)
+# equ2_point_in = (6500, 5000, 8000)
+# equ2_point_in_dir=(0,0,1)
+# equ2_point_out = (7000, 5000, 8500)
+# equ2_point_out_dir=(1,0,0)
+# equ2_length = 1000
+# equ2_width = 1000
+# equ2_height = 1000
 
 
 # # Equipment 3
