1f6975834414090fdd5ea8105e45e851d5c27b1e
Hallvaeb <hallvard.bjorgen@gmail.com>
2022-05-08 14:58:53 +0200

    name correction

--- UserInterface.py
+++ UserInterface.py
@@ -10,5 +10,5 @@ email = "your_email_please@gmail.com"
 
 # INPUT END --------------------------------------------------------------------
 
-env_args, equs_args, pipe_args = ParameterHolder.get_parameter_list(no_of_equs = specify_number_of_equipments_please)
+env_args, equs_args, pipe_args = ParameterContainer.get_parameter_list(no_of_equs = specify_number_of_equipments_please)
 Controller.construct(env_args, equs_args, pipe_args, email)
